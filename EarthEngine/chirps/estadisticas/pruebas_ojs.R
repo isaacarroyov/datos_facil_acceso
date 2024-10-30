b64bb58fc9edbdfbd154e081d71561880c853ad8
Isaac Arroyo <68873310+isaacarroyov@users.noreply.github.com>
2024-10-30 02:35:29 -0600

    Update pruebas_ojs - finished data processing - todo datavis
    Se deja TODO para continuar con la creacion de las visualizaciones

--- EarthEngine/chirps/estadisticas/pruebas_ojs.R
+++ EarthEngine/chirps/estadisticas/pruebas_ojs.R
@@ -13,6 +13,7 @@
 #'     fig-width: 5.5
 #'     fig-asp: 0.75
 #'     fig-dpi: 300
+#'     code-fold: true
 #'     code-annotations: below
 #'     code-line-numbers: true
 #' 
@@ -27,19 +28,23 @@
 #| eval: false
 #| echo: false
 
-setwd("./ignored_files")
+setwd("./EarthEngine/chirps/estadisticas")
 
+#' # Introducción
+#' 
 #' Para esta prueba se harán 3 visualizaciones 
 #' con [**ObservablePlot**](https://observablehq.com/plot):
 #' 
-#' 1. Anomalía de la precipitación anual: incluye valor nacional
-#' 3. Anomalía de la precipitación mensual: incluye valor nacional
-#' 2. Acumulación de la precipitación mensual: incluye el valor normal
+#' 1. Anomalía anual de la precipitación
+#' 2. Anomalía mensual de la precipitación
+#' 3. Acumulación mensual de la precipitación
+#' 
+#' La información se verá a partir del 2011 hasta la fecha más disponible.
 
-#| label: load_all_necesario
+#| label: load-paths2data
 library(tidyverse)
 
-path2main <- paste0(getwd(),"/.." )
+path2main <- paste0(getwd(), "/../../.." )
 path2ee <- paste0(path2main, "/EarthEngine")
 path2chirps <- paste0(path2ee, "/chirps")
 path2chirps_data <- paste0(path2chirps, "/data")
@@ -64,23 +69,109 @@ db_pr_normal_ent_month <- read_csv(
       path2chirps_datanormal,
       "/db_pr_normal_ent_month.csv"))
 
-#' ---
+#' # Procesamiento de datos
+#' 
+#' ## Anomalía anual de la precipitación
+#' 
+#' El objetivo es poder lograr una visualización que refleje la anomalía 
+#' de la lluvia al paso de los años en todo el país y en cada estado.
+#' 
+#' Para ello se necesita una estructura donde cada fila sea un estado 
+#' (se incluye la nación) en un determinado año con su respectivo valor de 
+#' anomalía.
 
-#| label: create-df2vis_anomaly_pr_prop_year
-df2vis_anomaly_pr_prop_year <- db_pr_ent_year %>%
+#| label: create-df2vis_info_ent_nac_year
+df2vis_info_ent_nac_year <- db_pr_ent_year %>%
   group_by(n_year) %>%
   summarise(pr_mm = mean(pr_mm)) %>%
   ungroup() %>%
   mutate(
     cve_ent = "00",
-    nombre_estado = "Nacional",
+    nombre_estado = "the country",
     normal_pr_mm = mean(db_pr_normal_ent_year$normal_pr_mm),
     anomaly_pr_mm = pr_mm - normal_pr_mm,
     anomaly_pr_prop = anomaly_pr_mm / normal_pr_mm) %>%
   select(!normal_pr_mm) %>%
   bind_rows(db_pr_ent_year)
 
+#' Un ejemplo de la visualización sería la siguiente
+
+#| label: fig-linechart-example-anomaly-year-ent-nac
+var_ent_name <- "Oaxaca"
+var_latest_update_info <- format(max(db_pr_ent_month$date_year_month), format = "%B %Y")
+
+df2vis_info_ent_nac_year %>%
+  ggplot(
+    mapping = aes(
+      x = n_year,
+      y = anomaly_pr_prop,
+      group = cve_ent,
+      color = if_else(
+        condition = nombre_estado == "the country",
+        true = "#BE3144",
+        false = "#240750"),
+      alpha = if_else(
+        condition = nombre_estado %in% c(var_ent_name, "the country"),
+        true = 1,
+        false = 0.3),
+      linewidth = if_else(
+        condition = nombre_estado %in% c(var_ent_name, "the country"),
+        true = 1.2,
+        false = 0.3))) +
+  geom_hline(yintercept = 0, color = "#000000", linewidth = 0.5) +
+  geom_line() +
+  scale_color_identity() +
+  scale_alpha_identity() +
+  scale_linewidth_identity() +
+  scale_x_continuous(breaks = seq(from = 2011, to = 2024, by = 3)) +
+  scale_y_continuous(
+    n.breaks = 6,
+    labels = scales::label_percent()) +
+  labs(
+    title = paste0("How much rain has <span style='color:#240750;'>",
+                   var_ent_name,"</span> had in recent years?" ),
+    subtitle = paste0("Yearly precipitation anomaly in ",
+                      "<b style='color:#BE3144;'>the country</b> compared to ",
+                      "the other <b style='color:#240750;'>states</b>.<br>",
+                      "The period used for computing the climatology is ",
+                      "1981-2010"),
+    caption = paste("Data: Climate Hazards Center InfraRed Precipitation",
+                    "With Station Data (CHIRPS).<br>",
+                    "Latest update:", var_latest_update_info, "<br>",
+                    "Chart: Isaac Arroyo (@isaacarroyov)")) +
+  theme_void() +
+  theme(
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.title = ggtext::element_textbox(
+      face = "bold",
+      width = unit(x = 5.5, units = "in")),
+    plot.subtitle = ggtext::element_textbox(
+      width = unit(x = 5.5, units = "in")),
+    plot.caption = ggtext::element_textbox(
+      width = unit(x = 5.5, units = "in")),
+    axis.line.x = element_line(color = "#000000"),
+    axis.ticks.length.x = unit(x = 0.05, units = "in"),
+    axis.ticks.x = element_line(color = "#000000"),
+    axis.text = element_text(),
+    panel.grid.major.y = element_line(color = "#000000", linewidth = 0.05)
+  )
+
+# tgutil::ggpreview(width = 5.5, height = 5.5 * 0.75, unit = "in", bg = "#F1EFE3")
+
+#' La principal idea, es que el valor de **`var_ent_name`** pueda ser 
+#' seleccionado a través de un menú desplegable. Gracias al menú el título 
+#' también cambia.
 #' 
+#' ## Anomalía mensual de la precipitación
+#' 
+#' El objetivo es poder lograr una visualización que refleje la anomalía de 
+#' cada mes del año de todos los años registrados de todo el país y 
+#' los estados.
+#' 
+#' Para ello se necesita una estructura donde cada fila sea un estado 
+#' (se incluye la nación) en un determinado año con su respectivo valor de 
+#' anomalía de un mes.
 
 #| label: create-df2vis_info_ent_nac_month
 df2vis_info_ent_nac_month <- db_pr_ent_month %>%
@@ -91,7 +182,7 @@ df2vis_info_ent_nac_month <- db_pr_ent_month %>%
   ungroup() %>%
   mutate(
     cve_ent = "00",
-    nombre_estado = "Nacional") %>%
+    nombre_estado = "the country") %>%
   left_join(
     y = db_pr_normal_ent_month %>%
           group_by(n_month) %>%
@@ -116,36 +207,261 @@ df2vis_info_ent_nac_month <- db_pr_ent_month %>%
   mutate(cumsum_pr_mm = cumsum(pr_mm)) %>%
   ungroup() %>%
   relocate(cumsum_pr_mm, .before = normal_pr_mm) %>%
-  select(!c(normal_pr_mm))
+  select(!c(normal_pr_mm)) %>%
+  mutate(n_year = as.character(n_year))
 
+#' Un ejemplo de la visualización sería la siguiente
 
+#| label: fig-linechart-example-anomaly-pr-prop-month-ent-nac
+var_n_year <- "2016"
+
+df2vis_info_ent_nac_month %>%
+  filter(n_year == var_n_year) %>%
+  ggplot(
+    mapping = aes(
+      x = n_month,
+      y = anomaly_pr_prop,
+      group = cve_ent,
+      color = if_else(
+        condition = nombre_estado == "the country",
+        true = "#BE3144",
+        false = "#240750"),
+      alpha = if_else(
+        condition = nombre_estado %in% c(var_ent_name, "the country"),
+        true = 1,
+        false = 0.3),
+      linewidth = if_else(
+        condition = nombre_estado %in% c(var_ent_name, "the country"),
+        true = 1.2,
+        false = 0.3))) +
+  geom_line() +
+  geom_hline(yintercept = 0, color = "#000000", linewidth = 0.5) +
+  geom_line() +
+  scale_color_identity() +
+  scale_alpha_identity() +
+  scale_linewidth_identity() +
+  scale_x_continuous(
+    breaks = 1:12,
+    labels = month.abb) +
+  scale_y_continuous(
+    n.breaks = 6,
+    labels = scales::label_percent(big.mark = ",")) +
+  coord_cartesian(xlim = c(1, 12)) +
+  labs(
+    title = paste0("How much rain did <span style='color:#240750;'>",
+                   var_ent_name,"</span> have in ", var_n_year, "?" ),
+    subtitle = paste0("Monthly precipitation anomaly in ",
+                      "<b style='color:#BE3144;'>the country</b> compared to ",
+                      "the other <b style='color:#240750;'>states</b>.<br>",
+                      "The period used for computing the climatology is ",
+                      "1981-2010"),
+    caption = paste("Data: Climate Hazards Center InfraRed Precipitation",
+                    "With Station Data (CHIRPS).<br>",
+                    "Latest update:", var_latest_update_info,"<br>",
+                    "Chart: Isaac Arroyo (@isaacarroyov)")) +
+  theme_void() +
+  theme(
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.title = ggtext::element_textbox(
+      face = "bold",
+      width = unit(x = 5.5, units = "in")),
+    plot.subtitle = ggtext::element_textbox(
+      width = unit(x = 5.5, units = "in")),
+    plot.caption = ggtext::element_textbox(
+      width = unit(x = 5.5, units = "in")),
+    axis.line.x = element_line(color = "#000000"),
+    axis.ticks.length.x = unit(x = 0.05, units = "in"),
+    axis.ticks.x = element_line(color = "#000000"),
+    axis.text = element_text(),
+    panel.grid.major.y = element_line(color = "#000000", linewidth = 0.05)
+  )
+
+# tgutil::ggpreview(width = 5.5, height = 5.5 * 0.75, unit = "in", bg = "#F1EFE3")
+
+#' La principal idea, es que el valor de **`var_ent_name`** y 
+#' **`var_n_year`** puedan ser seleccionados a través de un menú 
+#' desplegable (cada uno). Gracias a los menús el título también cambia.
 #' 
+#' ## Acumulación mensual de la precipitación
+#' 
+#' Para este último lo único que hace falta es poder comparar la 
+#' acumulación de la precipitación con la normal.
+#' 
+#' Para ello se agrega la información de la acumulación normal a 
+#' **`df2vis_info_ent_nac_month`**
 
+#| label: create-df2vis_info_cumsum_ent_nac_month
+df2vis_info_cumsum_ent_nac_month <- db_pr_normal_ent_month %>%
+  group_by(n_month) %>%
+  summarise(pr_mm = mean(normal_pr_mm)) %>%
+  ungroup() %>% 
+  mutate(
+    cumsum_pr_mm = cumsum(pr_mm),
+    n_month = as.numeric(n_month),
+    n_year = "1980-2010",
+    cve_ent = "00",
+    nombre_estado = "the country") %>%
+  select(
+    cve_ent,
+    nombre_estado,
+    n_year, n_month,
+    pr_mm,
+    cumsum_pr_mm) %>%
+  bind_rows(
+    db_pr_normal_ent_month %>%
+      mutate(n_month = as.numeric(n_month)) %>%
+      rename(pr_mm = normal_pr_mm) %>%
+      group_by(
+        cve_ent,
+        nombre_estado) %>%
+      arrange(n_month, .by_group = TRUE) %>%
+      mutate(cumsum_pr_mm = cumsum(pr_mm)) %>%
+      ungroup() %>%
+      mutate(n_year = "1980-2010") %>%
+      select(
+        cve_ent,
+        nombre_estado,
+        n_year, n_month,
+        pr_mm,
+        cumsum_pr_mm)) %>%
+  bind_rows(
+    df2vis_info_ent_nac_month %>% 
+      select(
+        cve_ent,
+        nombre_estado,
+        n_year, n_month,
+        pr_mm,
+        cumsum_pr_mm))
 
+#' Un ejemplo de la visualización sería la siguiente
+
+#| label: fig-linechart-example-cumsum-nac-ent-year-month
+df2vis_info_cumsum_ent_nac_month %>%
+  filter(nombre_estado == var_ent_name) %>%
+  ggplot(
+    mapping = aes(
+      x = n_month,
+      y = cumsum_pr_mm,
+      group = n_year,
+      color = if_else(
+        condition = n_year == "1980-2010",
+        true = "#27374D",
+        false = "#116D6E"),
+      alpha = if_else(
+        condition = n_year %in% c(var_n_year, "1980-2010"),
+        true = 1,
+        false = 0.3),
+      linewidth = if_else(
+        condition = n_year %in% c(var_n_year, "1980-2010"),
+        true = 1.2,
+        false = 0.3))) +
+  geom_line() +
+  geom_line() +
+  scale_color_identity() +
+  scale_alpha_identity() +
+  scale_linewidth_identity() +
+  scale_x_continuous(
+    breaks = 1:12,
+    labels = month.abb) +
+  scale_y_continuous(labels = scales::label_comma(suffix = " mm")) +
+  coord_cartesian(xlim = c(1, 12)) +
+  labs(
+    title = paste0("Was there enough rain in ", var_ent_name, " during ",
+                   "<span style='color:#116D6E;'>", var_n_year, "</span>?" ),
+    subtitle = paste0("Monthly precipitation accumulation ",
+                      "compared to the <b style='color:#27374D;'>",
+                      "1980-2010 period</b>."),
+    caption = paste("Data: Climate Hazards Center InfraRed Precipitation",
+                    "With Station Data (CHIRPS).<br>",
+                    "Latest update:", var_latest_update_info,".<br>",
+                    "Chart: Isaac Arroyo (@isaacarroyov)")) +
+  theme_void() +
+  theme(
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.title = ggtext::element_textbox(
+      face = "bold",
+      width = unit(x = 5.5, units = "in")),
+    plot.subtitle = ggtext::element_textbox(
+      width = unit(x = 5.5, units = "in")),
+    plot.caption = ggtext::element_textbox(
+      width = unit(x = 5.5, units = "in")),
+    axis.line.x = element_line(color = "#000000"),
+    axis.ticks.length.x = unit(x = 0.05, units = "in"),
+    axis.ticks.x = element_line(color = "#000000"),
+    axis.text = element_text(),
+    panel.grid.major.y = element_line(color = "#000000", linewidth = 0.05)
+  )
+
+# tgutil::ggpreview(width = 5.5, height = 5.5 * 0.75, unit = "in", bg = "#F1EFE3")
+
+#' La principal idea, es que el valor de **`var_ent_name`** y 
+#' **`var_n_year`** puedan ser seleccionados a través de un menú 
+#' desplegable (cada uno). Gracias a los menús el título también cambia.
+#' 
+#' # Creación de visualizaciones con ObservablePlot
+#' 
+#' ## Transformación a objetos para ObservablePlot
+#' 
+#' Para poder crear todas las visualizaciones, hace falta hacer la 
+#' conversión de datos _tidy_ al formato usado por ObservablePlot. Esta 
+#' tarea se hará con la función **`ojs_define`**^[Función para la 
+#' transformación de `pandas.DataFrame` de **Python** y `tibble` de **R** a 
+#' datos para ObservablePlot. Solo funciona cuando se renderiza el script].
+
+#| code-summary: "Transform data"
 #| label: create-observable-objects-data2vis
-#| echo: true
-ojs_define(data2vis = datos_interes)
-ojs_define(latest_info = format(max(datos_interes$date_year_month), format = "%B %Y"))
+ojs_define(data2vis_year = df2vis_info_ent_nac_year)
+ojs_define(data2vis_month = df2vis_info_ent_nac_month)
+ojs_define(data2vis_cumsum_month = df2vis_info_cumsum_ent_nac_month)
 
+#' También es importante poder importar todos los parámetros que serán 
+#' necesario para los menús, así como para mostar la más reciente 
+#' actualización de los datos.
+
+#| code-summary: "Create elements"
+#| label: create-observable-objects-arrays4menus-and-strings
+
+ojs_define(array_ent_name = pull(
+    .data = distinct(
+      .data = df2vis_info_cumsum_ent_nac_month,
+      nombre_estado)))
+
+ojs_define(array_n_year = pull(
+    .data = distinct(
+      .data = df2vis_info_ent_nac_month,
+      n_year)))
+
+ojs_define(string_latest_update = format(
+    x = max(db_pr_ent_month$date_year_month),
+    format = "%B %Y"))
+
+#' ## Creación de menús desplegables
 #' 
-
-#| label: create-observable-objects-array_ent_name
-#| echo: true
-ojs_define(array_ent_name = pull(distinct(datos_interes, nombre_estado)))
-
+#' Con las transformaciones hechas, ahora podemos crear los menús 
+#' desplegables
+#' 
+#' **Menú de nombre de entidades**
 
 #' ```{ojs}
 #' viewof select_ent_name = Inputs.select(
 #'     array_ent_name,
 #'     {value: 'Yucatán',
-#'     label: "Estado"})
+#'     label: "State"})
 #' ```
-
-#| label: create-observable-objects-array_n_year
-#| echo: true
-ojs_define(array_n_year = pull(distinct(datos_interes, n_year)))
-
-
+#' 
+#' **Menú de años**
+#' 
+#' ```{ojs}
+#' viewof select_n_year = Inputs.select(
+#'     array_n_year,
+#'     {value: "2023",
+#'     label: "Year"})
+#' ```
+#' 
+#' TODO: Continuar here
+#' 
 #' ```{ojs}
 #' viewof select_n_year = Inputs.select(
 #'     array_n_year,
